b74b32dbd53d07796891caf129ecb52c3a67f6f5
Wardah Jabeen <jabeen@usc.edu>
2023-11-09 18:45:57 -0800

    router setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./Pages/Home"
+import Profile from "./Pages/Profile"
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Link, Outlet } from 'react-router-dom'
 
 function App() {
+
+  // Router set up version6
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+    )
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
+}
+
+// Router set up version6
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
 }
 
 export default App;
